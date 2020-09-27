21ce3f6a29308148af482fcc4ec477c317f07cf7
charan <charan9487@gmail.com>
2020-09-27 11:16:30 +0530

    added lazy suspense to category page

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
-import CategoryPageContainer from "../category/category.container";
+import Spinner from "../../components/spinner/spinner";
 import { connect } from "react-redux";
 
 import { updateLoading } from "../../redux/spinner/spinner.action";
@@ -8,7 +8,9 @@ import { updateLoading } from "../../redux/spinner/spinner.action";
 import CollectionOverviewContainer from "../../components/collection-overview/collection-overview.container";
 
 import { fetchCollectionAsync } from "../../redux/shop/shop.action";
-
+const CategoryPageContainer = lazy(() =>
+  import("../category/category.container")
+);
 const ShopPage = ({ fetchCollectionAsync, match }) => {
   useEffect(() => {
     fetchCollectionAsync();
@@ -21,11 +23,12 @@ const ShopPage = ({ fetchCollectionAsync, match }) => {
         path={`${match.path}`}
         component={CollectionOverviewContainer}
       />
-
-      <Route
-        path={`${match.path}/:categoryId`}
-        component={CategoryPageContainer}
-      />
+      <Suspense fallback={<Spinner />}>
+        <Route
+          path={`${match.path}/:categoryId`}
+          component={CategoryPageContainer}
+        />
+      </Suspense>
     </div>
   );
 };
