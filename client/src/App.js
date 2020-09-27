24b8de2e4a705d2d1b4d9a63d27e82a0b6b9c888
charan <charan9487@gmail.com>
2020-09-27 11:02:35 +0530

    added react lazy suspense to shop signin page

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,21 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { GlobalStyle } from './global.styles'
 //import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import Header from './components/header/header.component';
 
 import { createStructuredSelector } from 'reselect'
 import { selectCurrentUser } from './redux/user/user.selector'
-import Checkout from './pages/checkout/checkout';
+
 
 import { checkUserSession } from './redux/user/user.action'
+
+
 const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const Checkout = lazy(() => import('./pages/checkout/checkout'))
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'))
+
 const App = ({ checkUserSession, currentUser }) => {
 
   useEffect(() => {
@@ -27,11 +32,13 @@ const App = ({ checkUserSession, currentUser }) => {
       <Switch>
         <Suspense fallback={<div>...Loading</div>}>
           <Route exact path='/' component={HomePage} />
+
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={Checkout} />
+          <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)}
+          />
         </Suspense>
-        <Route path='/shop' component={ShopPage} />
-        <Route exact path='/checkout' component={Checkout} />
-        <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)}
-        />
+
       </Switch>
     </div>
   );
